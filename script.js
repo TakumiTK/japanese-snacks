2da4d44d204b70e74f557a51fc54fab2f5d87d37
TakumiTK <92070796+TakumiTK@users.noreply.github.com>
2023-12-30 16:06:10 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+/*index.html */
+window.addEventListener('load', function () {
+   viewSlide('.slide img');
+});
+
+function viewSlide(className, slideNo = -1) {
+   let imgArray = document.querySelectorAll(className);
+   if (slideNo >= 0) {
+      imgArray[slideNo].style.opacity = 0;
+   }
+   slideNo++;
+   if (slideNo >= imgArray.length) {
+      slideNo = 0;
+   }
+   imgArray[slideNo].style.opacity = 1;
+   // 時間を固定したい場合、ここでミリ秒を指定する
+   let msec = 4000; //4秒
+   setTimeout(function () {
+      viewSlide(className, slideNo);
+   }, msec);
+}
+
+
